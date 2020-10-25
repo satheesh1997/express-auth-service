422753621a23209243dc4ca0841ecd214f38d464
Suresh Kumar <mail.sk1999@gmail.com>
2020-10-26 04:13:25 +0530

    login_viewe: added login page

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+
+
+function login(emailElement, passwordElement) {
+    if(!$('#errorAlert').hasClass('invisible'))
+        $('#errorAlert').addClass('invisible');
+    let loginBtn = $("#loginbtn");
+    let loginSpin = $("#loginbtn span");
+
+    loginBtn.prop('disabled', true);
+    loginSpin.toggleClass('myFade');
+
+    let email = emailElement.val();
+    let password = passwordElement.val();
+    $.ajax({
+        method: 'post',
+        url: '/users/login/',
+        contentType: 'application/json',
+        data: JSON.stringify({ email, password})
+    }). done(function(){
+        console.log('Success');
+    }) .fail(function(){
+        $('#errorAlert').removeClass('invisible');
+        loginSpin.toggleClass('myFade');
+    })
+}
